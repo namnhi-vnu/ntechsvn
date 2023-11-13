77fdc6367bf6544ee0aa259a923dd954496c785c
namnhi-vnu <namnhi.vnu@gmail.com>
2023-11-13 17:47:12 +0700

    update two

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                ntechs: "#084183",
+                hover: "#EB681B",
+                "black-rgba": "rgba(235,104,27,.4)",
+                "bg-rgba": "rgba(235,104,27,.2)",
+            },
+            minWidth: {
+                menu: "200px",
+            },
+            lineHeight: {
+                13: "3.5rem",
+                12: "1.4em",
+            },
+            fontSize: {
+                "5ml": "2.802rem",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
