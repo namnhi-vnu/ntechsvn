8cd32255004ae26d3c1d1d512b67b0ba52aa9069
namnhi-vnu <namnhi.vnu@gmail.com>
2023-11-13 20:38:25 +0700

    update font

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,10 @@ export default function RootLayout({ children }) {
                     rel="stylesheet"
                     href="https://unpkg.com/aos@next/dist/aos.css"
                 />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Agbalumo&family=Alumni+Sans+Collegiate+One:ital@0;1&family=Black+Ops+One&family=Chivo+Mono:wght@100;200;300;400;500;600;700;800;900&family=Codystar:wght@300;400&family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&family=Kablammo&family=Kanit:wght@100;200;300;400;500;600;700;800&family=Lilita+One&family=Nunito+Sans:wght@200;300;400;600;700;800;900&family=Playpen+Sans:wght@200;300;400;500;600;700;800&family=Roboto+Slab:wght@200;300;400&family=Roboto:wght@100;300;400;500;700;900&family=Special+Elite&display=swap"
+                    rel="stylesheet"
+                ></link>
                 {/* <link
                     rel="shortcut icon"
                     href="/icon-logo.png"
